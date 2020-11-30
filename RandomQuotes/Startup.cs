5eb2a0b50499145b4cb4228d4a872d729ba5df77
Juan Carlos Garcia <juan.garcia@bolivariano.com.co>
2020-11-30 15:36:08 -0500

    Net 5.0 + JK 01

--- RandomQuotes/Startup.cs
+++ RandomQuotes/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using RandomQuotes.Models;
 
 namespace RandomQuotes
@@ -27,7 +28,7 @@ namespace RandomQuotes
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
